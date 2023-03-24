f496dff742b0854948712d19b201cbae47e44afa
bator <batr.fly@mail.ru>
2023-03-24 08:28:46 +0400

    fix header styles

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -10,40 +10,42 @@ import s from './app-header.module.css';
 function AppHeader() {
   return (
     <header className={s.header}>
-      <ul className={s.list}>
-        <li>
-          <Button
-            htmlType="button"
-            type="secondary"
-            size="medium"
-            extraClass={s.button + ' text text_color_inactive'}
-          >
-            <BurgerIcon type="secondary" />
-            Конструктор
-          </Button>
-        </li>
-        <li>
-          <Button
-            htmlType="button"
-            type="secondary"
-            size="medium"
-            extraClass={s.button + ' text text_color_inactive'}
-          >
-            <ListIcon type="secondary" />
-            Лента заказов
-          </Button>
-        </li>
-      </ul>
-      <Logo />
-      <Button
-        htmlType="button"
-        type="secondary"
-        size="medium"
-        extraClass={s.button + ' text text_color_inactive ' + s.profile}
-      >
-        <ProfileIcon type="secondary" />
-        Личный кабинет
-      </Button>
+      <div className={s.container}>
+        <ul className={s.list}>
+          <li>
+            <Button
+              htmlType="button"
+              type="secondary"
+              size="medium"
+              extraClass={s.button + ' text text_color_inactive'}
+            >
+              <BurgerIcon type="secondary" />
+              Конструктор
+            </Button>
+          </li>
+          <li>
+            <Button
+              htmlType="button"
+              type="secondary"
+              size="medium"
+              extraClass={s.button + ' text text_color_inactive'}
+            >
+              <ListIcon type="secondary" />
+              Лента заказов
+            </Button>
+          </li>
+        </ul>
+        <Logo />
+        <Button
+          htmlType="button"
+          type="secondary"
+          size="medium"
+          extraClass={s.button + ' text text_color_inactive ' + s.profile}
+        >
+          <ProfileIcon type="secondary" />
+          Личный кабинет
+        </Button>
+      </div>
     </header>
   );
 }
