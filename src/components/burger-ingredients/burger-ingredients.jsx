0fcc94d1e6f5f82fb53f1d73a62cd4923989edfb
bator <batr.fly@mail.ru>
2023-04-01 16:32:19 +0400

    added ingredient details

---
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -0,0 +1,105 @@
+import { Tab } from '@ya.praktikum/react-developer-burger-ui-components';
+import React, { useState } from 'react';
+import s from './burger-ingredients.module.css';
+import { BurgerIngredient } from '../burger-ingredient/burger-ingredient';
+import { ingredientsTypes } from '../../utils/prop-types';
+import IngredientDetails from '../ingredient-details/ingredient-details';
+export function BurgerIngredients({ ingredients }) {
+  const [current, setCurrent] = useState('bun');
+  const [visible, setVisible] = useState(false);
+  const [ingredient, setIngredient] = useState();
+  const buns = ingredients.filter((item) => item.type === 'bun');
+  const sauces = ingredients.filter((item) => item.type === 'sauce');
+  const mains = ingredients.filter((item) => item.type === 'main');
+  const handleOpenModal = () => setVisible(true);
+  const handleCloseModal = () => setVisible(false);
+  const modal = () => (
+    <IngredientDetails
+      ingredient={ingredient}
+      handleClick={handleCloseModal}
+    />
+  );
+  const handleOpenIngredientModal = (id) => {
+    const currentIngredient = ingredients.find((item) => item._id === id);
+    setIngredient(() => currentIngredient);
+    handleOpenModal();
+  };
+  return (
+    <section className={s.burger}>
+      <h1 className="text text_type_main-large mb-5">Соберите бургер</h1>
+      <ul className={s.tabs}>
+        <li>
+          <Tab
+            value="bun"
+            active={current === 'bun'}
+            onClick={setCurrent}
+          >
+            Булки
+          </Tab>
+        </li>
+        <li>
+          <Tab
+            value="sauce"
+            active={current === 'sauce'}
+            onClick={setCurrent}
+          >
+            Соусы
+          </Tab>
+        </li>
+        <li>
+          <Tab
+            value="main"
+            active={current === 'main'}
+            onClick={setCurrent}
+          >
+            Начинки
+          </Tab>
+        </li>
+      </ul>
+      <ul className={s.ingridients + ' custom-scroll'}>
+        <li className="mt-10">
+          <h2 className="text text_type_main-medium">Булки</h2>
+          <ul className={s.ingridient_items}>
+            {buns &&
+              buns.map((bun) => (
+                <BurgerIngredient
+                  handleClick={handleOpenIngredientModal}
+                  ingredient={bun}
+                  key={bun._id}
+                />
+              ))}
+          </ul>
+        </li>
+        <li className="mt-10">
+          <h2 className="text text_type_main-medium">Соусы</h2>
+          <ul className={s.ingridient_items}>
+            {sauces &&
+              sauces.map((sauce) => (
+                <BurgerIngredient
+                  ingredient={sauce}
+                  key={sauce._id}
+                />
+              ))}
+          </ul>
+        </li>
+        <li className="mt-10">
+          <h2 className="text text_type_main-medium">Начинки</h2>
+          <ul className={s.ingridient_items}>
+            {mains &&
+              mains.map((main) => (
+                <BurgerIngredient
+                  ingredient={main}
+                  key={main._id}
+                />
+              ))}
+          </ul>
+        </li>
+      </ul>
+      {visible && modal()}
+    </section>
+  );
+}
+
+BurgerIngredients.propTypes = {
+  ingredients: ingredientsTypes,
+};
---
+++ src/components/ingredient-details/ingredient-details.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Modal from '../modal/modal';
+import s from './ingredient-details.module.css';
+
+export default function IngredientDetails({ ingredient, handleClick }) {
+  const { image_large, calories, name, proteins, fat, carbohydrates } =
+    ingredient;
+  return (
+    <Modal handleClick={handleClick}>
+      <p className="text text_type_main-large">Детали ингредиента</p>
+      <img
+        width={520}
+        height={240}
+        src={image_large}
+        alt=""
+      />
+      <p className={s.name + ' text text_type_main-default'}>{name}</p>
+      <ul className={s.values}>
+        <li className={s.value}>
+          <p className="text text_type_main-default">Калории,ккал</p>
+          <p className="text text_type_digits-default">{calories}</p>
+        </li>
+        <li className={s.value}>
+          <p className="text text_type_main-default">Белки, г</p>
+          <p className="text text_type_digits-default">{proteins}</p>
+        </li>
+        <li className={s.value}>
+          <p className="text text_type_main-default">Жиры, г</p>
+          <p className="text text_type_digits-default">{fat}</p>
+        </li>
+        <li className={s.value}>
+          <p className="text text_type_main-default">Углеводы, г</p>
+          <p className="text text_type_digits-default">{carbohydrates}</p>
+        </li>
+      </ul>
+    </Modal>
+  );
+}
