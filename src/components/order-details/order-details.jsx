dc18a2921770f5e3e3cc09e429e353ce720193d6
bator <batr.fly@mail.ru>
2023-04-01 17:19:05 +0400

    added order details

---
+++ src/components/order-details/order-details.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Modal from '../modal/modal';
+import s from './order-details.module.css';
+import doneImage from '../../images/done.jpg';
+
+export function OrderDetails({ handleCloseModal }) {
+  return (
+    <Modal handleCloseModal={handleCloseModal}>
+      <div className={s.order}>
+        <p className="text text_type_digits-large">034536</p>
+        <p className="text text_type_main-medium mt-8">идентификатор заказа</p>
+        <img
+          className={s.image}
+          src={doneImage}
+          alt=""
+        />
+        <p className="text text_type_main-default mb-2">
+          Ваш заказ начали готовить
+        </p>
+        <p className={s.colored + ' text text_type_main-default'}>
+          Дождитесь готовности на орбитальной станции
+        </p>
+      </div>
+    </Modal>
+  );
+}
