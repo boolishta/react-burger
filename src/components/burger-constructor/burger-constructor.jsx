cc995bccf4d1f86625587fe84d8ff75676ee1b1c
bator <batr.fly@mail.ru>
2023-04-03 10:05:39 +0400

    refactor burger constructor

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -19,15 +19,7 @@ export function BurgerConstructor({ ingredients }) {
       <OrderDetails />
     </Modal>
   );
-  const computedConstructorElementType = (index) => {
-    if (index === 0) {
-      return 'top';
-    } else if (index === ingredients.length - 1) {
-      return 'bottom';
-    }
-    return null;
-  };
-
+  const buns = ingredients.filter((item) => item.type === 'bun');
   const handleClick = () => {
     handleOpenModal();
   };
@@ -36,24 +28,43 @@ export function BurgerConstructor({ ingredients }) {
     <>
       <div>
         <ul className={s.elements + ' custom-scroll'}>
-          {ingredients.map((ingredient, index) => (
-            <li
-              key={ingredient._id}
-              className={s.element}
-            >
-              {index > 0 && index < ingredients.length - 1 ? (
-                <DragIcon type="primary" />
-              ) : null}
-              <ConstructorElement
-                extraClass={s.constructorElement}
-                isLocked={ingredient.type === 'bun'}
-                text={ingredient.name}
-                price={ingredient.price}
-                thumbnail={ingredient.image}
-                type={computedConstructorElementType(index)}
-              />
-            </li>
-          ))}
+          <li className={s.element}>
+            <ConstructorElement
+              extraClass={s.constructorElement}
+              text={buns[0].name}
+              price={buns[0].price}
+              thumbnail={buns[0].image}
+              isLocked={true}
+              type="top"
+            />
+          </li>
+          {ingredients.map(
+            (ingredient) =>
+              ingredient.type !== 'bun' && (
+                <li
+                  key={ingredient._id}
+                  className={s.element}
+                >
+                  <DragIcon type="primary" />
+                  <ConstructorElement
+                    extraClass={s.constructorElement}
+                    text={ingredient.name}
+                    price={ingredient.price}
+                    thumbnail={ingredient.image}
+                  />
+                </li>
+              )
+          )}
+          <li className={s.element}>
+            <ConstructorElement
+              extraClass={s.constructorElement}
+              text={buns[1].name}
+              price={buns[1].price}
+              thumbnail={buns[1].image}
+              isLocked={true}
+              type="bottom"
+            />
+          </li>
         </ul>
         <div className={s.info}>
           <Price />
