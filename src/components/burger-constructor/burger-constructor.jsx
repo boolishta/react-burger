242e47d271ed46159f8dd83949cc615e2d8ca26b
bator <batr.fly@mail.ru>
2023-04-04 09:22:25 +0400

    refactor burder constructor

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -6,7 +6,7 @@ import {
 import s from './burger-constructor.module.css';
 import Price from '../price/price';
 import { ingredientsType } from '../../utils/prop-types';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import { OrderDetails } from '../order-details/order-details';
 import Modal from '../modal/modal';
 
@@ -19,7 +19,9 @@ export function BurgerConstructor({ ingredients }) {
       <OrderDetails />
     </Modal>
   );
-  const buns = ingredients.filter((item) => item.type === 'bun');
+  const buns = useMemo(() => {
+    return ingredients.filter((item) => item.type === 'bun');
+  }, [ingredients]);
   const handleClick = () => {
     handleOpenModal();
   };
@@ -28,16 +30,18 @@ export function BurgerConstructor({ ingredients }) {
     <>
       <div>
         <ul className={s.elements + ' custom-scroll'}>
-          <li className={s.element}>
-            <ConstructorElement
-              extraClass={s.constructorElement}
-              text={buns[0].name}
-              price={buns[0].price}
-              thumbnail={buns[0].image}
-              isLocked={true}
-              type="top"
-            />
-          </li>
+          {buns[0] && (
+            <li className={s.element}>
+              <ConstructorElement
+                extraClass={s.constructorElement}
+                text={buns[0]?.name + ' (верх)'}
+                price={buns[0]?.price}
+                thumbnail={buns[0]?.image}
+                isLocked={true}
+                type="top"
+              />
+            </li>
+          )}
           {ingredients.map(
             (ingredient) =>
               ingredient.type !== 'bun' && (
@@ -55,16 +59,18 @@ export function BurgerConstructor({ ingredients }) {
                 </li>
               )
           )}
-          <li className={s.element}>
-            <ConstructorElement
-              extraClass={s.constructorElement}
-              text={buns[1].name}
-              price={buns[1].price}
-              thumbnail={buns[1].image}
-              isLocked={true}
-              type="bottom"
-            />
-          </li>
+          {buns[1] && (
+            <li className={s.element}>
+              <ConstructorElement
+                extraClass={s.constructorElement}
+                text={buns[1]?.name + ' (низ)'}
+                price={buns[1]?.price}
+                thumbnail={buns[1]?.image}
+                isLocked={true}
+                type="bottom"
+              />
+            </li>
+          )}
         </ul>
         <div className={s.info}>
           <Price />
