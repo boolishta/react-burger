74179fafad62e8c6be5c5b749432527e7a228b0b
bator <batr.fly@mail.ru>
2023-04-12 09:45:40 +0400

    fix files

---
+++ src/components/app-header/app-header.jsx
@@ -0,0 +1,51 @@
+import {
+  BurgerIcon,
+  Button,
+  ListIcon,
+  Logo,
+  ProfileIcon,
+} from '@ya.praktikum/react-developer-burger-ui-components';
+import s from './app-header.module.css';
+
+export function AppHeader() {
+  return (
+    <header className={s.header}>
+      <nav className={s.container}>
+        <ul className={s.list}>
+          <li>
+            <Button
+              htmlType="button"
+              type="secondary"
+              size="medium"
+              extraClass={s.button + ' text text_color_inactive'}
+            >
+              <BurgerIcon type="secondary" />
+              Конструктор
+            </Button>
+          </li>
+          <li>
+            <Button
+              htmlType="button"
+              type="secondary"
+              size="medium"
+              extraClass={s.button + ' text text_color_inactive'}
+            >
+              <ListIcon type="secondary" />
+              Лента заказов
+            </Button>
+          </li>
+        </ul>
+        <Logo />
+        <Button
+          htmlType="button"
+          type="secondary"
+          size="medium"
+          extraClass={s.button + ' text text_color_inactive ' + s.profile}
+        >
+          <ProfileIcon type="secondary" />
+          Личный кабинет
+        </Button>
+      </nav>
+    </header>
+  );
+}
