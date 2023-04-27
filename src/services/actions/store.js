d2bb6e8dde93f3a5b837aa62ee34787d9406814a
bator <batr.fly@mail.ru>
2023-04-27 09:08:12 +0200

    added ingredient details action reducer

--- src/services/actions/store.js
+++ src/services/actions/store.js
@@ -3,6 +3,9 @@ import { fetchIngredients } from '../../utils/burger-api';
 export const GET_INGREDIENTS_REQUEST = 'GET_INGREDIENTS_REQUEST';
 export const GET_INGREDIENTS_SUCCESS = 'GET_INGREDIENTS_SUCCESS';
 export const GET_INGREDIENTS_FAILED = 'GET_INGREDIENTS_FAILED';
+export const GET_CURRENT_INGREDIENTS = 'GET_CURRENT_INGREDIENTS';
+export const ADD_INGREDIENT_DETAILS = 'ADD_INGREDIENT_DETAILS';
+export const CLEAR_INGREDIENT_DETAILS = 'CLEAR_INGREDIENT_DETAILS';
 
 export function getIngredients() {
   return function (dispatch) {
--- src/services/reducers/store.js
+++ src/services/reducers/store.js
@@ -2,6 +2,8 @@ import {
   GET_INGREDIENTS_REQUEST,
   GET_INGREDIENTS_SUCCESS,
   GET_INGREDIENTS_FAILED,
+  ADD_INGREDIENT_DETAILS,
+  CLEAR_INGREDIENT_DETAILS,
 } from '../actions/store';
 
 const initialStore = {
@@ -9,12 +11,26 @@ const initialStore = {
   ingredientsRequest: false,
   ingredientsFailed: false,
   currentIngredients: [],
-  currentIngredient: {},
+  ingredientDetails: {},
   order: {},
 };
 
 export const storeReducer = (state = initialStore, action) => {
   switch (action.type) {
+    case CLEAR_INGREDIENT_DETAILS: {
+      return {
+        ...state,
+        ingredientDetails: initialStore.ingredientDetails,
+      };
+    }
+    case ADD_INGREDIENT_DETAILS: {
+      return {
+        ...state,
+        ingredientDetails: state.ingredients.find(
+          (ingredient) => ingredient._id === action.ingredientId
+        ),
+      };
+    }
     case GET_INGREDIENTS_REQUEST: {
       return {
         ...state,
