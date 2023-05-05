39939cf869dd83acb612e5380e58aa02fad92efa
bator <batr.fly@mail.ru>
2023-05-05 09:35:39 +0200

    refactor total price

--- src/components/price/price.jsx
+++ src/components/price/price.jsx
@@ -1,10 +1,20 @@
 import { CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
-import React from 'react';
+import React, { useMemo } from 'react';
 import { useSelector } from 'react-redux';
 
 export default function Price() {
   const { currentIngredients } = useSelector((state) => state.store);
-  const total = currentIngredients.reduce((acc, item) => acc + item.price, 0);
+  const total = useMemo(
+    () =>
+      currentIngredients.reduce((acc, item) => {
+        let price = item.price;
+        if (item.type === 'bun') {
+          price += item.price;
+        }
+        return acc + price;
+      }, 0),
+    [currentIngredients]
+  );
 
   return (
     <p className="text text_type_digits-medium">
