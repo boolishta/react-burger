d6f9319ab91dc1ad21a5a202b82c20e14aba2597
bator <batr.fly@mail.ru>
2023-05-14 12:31:10 +0200

    add home page

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,21 +1,16 @@
 import React from 'react';
-import { AppHeader } from '../app-header/app-header';
-import { BurgerConstructor } from '../burger-constructor/burger-constructor';
-import { BurgerIngredients } from '../burger-ingredients/burger-ingredients';
-import s from './app.module.css';
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HomePage } from '../../pages';
 
 export function App() {
   return (
-    <>
-      <AppHeader />
-      <main className={s.order}>
-        <DndProvider backend={HTML5Backend}>
-          <BurgerIngredients />
-          <BurgerConstructor />
-        </DndProvider>
-      </main>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={<HomePage />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
