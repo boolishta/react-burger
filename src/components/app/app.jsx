8677336135615c0561df23a894ae53a3b1f03280
bator <batr.fly@mail.ru>
2023-05-15 07:42:46 +0200

    add login page

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { HomePage } from '../../pages';
+import { HomePage, LoginPage } from '../../pages';
 
 export function App() {
   return (
@@ -9,6 +9,10 @@ export function App() {
         <Route
           path="/"
           element={<HomePage />}
+        />
+        <Route
+          path="/login"
+          element={<LoginPage />}
         />
       </Routes>
     </BrowserRouter>
---
+++ src/pages/login.jsx
@@ -0,0 +1,78 @@
+import {
+  Button,
+  EmailInput,
+  PasswordInput,
+} from '@ya.praktikum/react-developer-burger-ui-components';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { AppHeader } from '../components/app-header/app-header';
+import s from './login.module.css?module';
+
+export function LoginPage() {
+  const [formValues, setFormValues] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormValues(() => ({
+      ...formValues,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <>
+      <AppHeader />
+      <div className={s.login}>
+        <form className={s.form}>
+          <p className="text text_type_main-medium">Вход</p>
+          <EmailInput
+            value={formValues.email}
+            placeholder="E-mail"
+            type="email"
+            name="email"
+            onChange={handleChange}
+          />
+          <PasswordInput
+            value={formValues.password}
+            name="password"
+            placeholder="Password"
+            onChange={handleChange}
+          />
+          <Button
+            htmlType="submit"
+            type="primary"
+            size="medium"
+            extraClass={s.button}
+          >
+            Войти
+          </Button>
+        </form>
+        <p
+          className={
+            s.register + ' text text_type_main-default text_color_inactive'
+          }
+        >
+          Вы — новый пользователь?{' '}
+          <Link
+            className={s.link}
+            to="/register"
+          >
+            Зарегистрироваться
+          </Link>
+        </p>
+        <p className="text text_type_main-default text_color_inactive">
+          Забыли пароль?{' '}
+          <Link
+            className={s.link}
+            to="/forgot-password"
+          >
+            Восстановить пароль
+          </Link>
+        </p>
+      </div>
+    </>
+  );
+}
