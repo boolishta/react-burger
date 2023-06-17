f4d7454b668543fe09e480d55efe750b1c41f129
bator <batr.fly@mail.ru>
2023-06-17 14:41:37 +0200

    web socket

--- src/pages/feed.jsx
+++ src/pages/feed.jsx
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { AppHeader } from '../components/app-header/app-header';
 import s from './feed.module.css';
 import Stats from '../components/stats/stats';
-import Orders from '../components/orders/orders';
+import Feeds from '../components/feeds/feeds';
+import { useDispatch } from 'react-redux';
+import { WS_CONNECTION_START } from '../redux/actions';
 
 export default function FeedPage() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch({
+      type: WS_CONNECTION_START,
+    });
+  }, [dispatch]);
   return (
     <>
       <AppHeader />
       <main className={s.feed + ' mt-10'}>
         <p className="text text_type_main-large">Лента заказов</p>
         <div className={s.columns + ' mt-5'}>
-          {/* TODO: тут будут Feeds где показываются все заказы */}
-          <Orders />
+          <Feeds />
           <Stats />
         </div>
       </main>
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,10 +3,12 @@ import { ingredientsReducer } from './ingredients';
 import { orderReducer } from './order';
 import { ingredientDetailsReducer } from './ingredientDetails';
 import { userReducer } from './user';
+import { wsReducer } from './wsReducer';
 
 export const rootReducer = combineReducers({
   ingredientDetails: ingredientDetailsReducer,
   ingredients: ingredientsReducer,
   order: orderReducer,
   user: userReducer,
+  ws: wsReducer,
 });
